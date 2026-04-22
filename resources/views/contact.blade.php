8636a60e2e364e33f9d594d7c054e40fc00fe795
Nico Deblauwe <nico@bluepundit.eu>
2026-04-22 14:20:39 +0200

    CONTENT :pencil: Add a contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>KIES IETS</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body class="">
+<nav>
+    <a class="mr-4" href="/">Welcome</a>
+    <a class="mr-4" href="/contact">Contact</a>
+</nav>
+
+<h1 class="text-2xl mb-4 font-bold">Contact</h1>
+<p>Mail mij op <a class="underline" href="mailto:nico.deblauwe@ehb.be">nico.deblauwe@ehb.be</a></p>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index']);
+Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'index']);
