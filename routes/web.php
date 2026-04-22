539467eb612664b842235e2a8bacfa491edcca58
Nico Deblauwe <nico@bluepundit.eu>
2026-04-22 14:20:59 +0200

    CONTENT :pencil: Add big groups in routes file

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+ * Public routes
+ */
 Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index']);
 Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'index']);
+
+/*
+ * User routes
+ */
+
+
+/*
+ * Admin routes
+ */
